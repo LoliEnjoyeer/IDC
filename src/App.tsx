26ced4fd2a7e93abc084cf7718cdd62d4f028c21
LoliEnjoyeer <104514546+LoliEnjoyeer@users.noreply.github.com>
2022-12-02 17:04:20 +0100

    template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Counter } from './features/counter/Counter'
+
+const App: React.FC = () => {
+    return (
+        <>
+            <Counter />
+            <h1>Hello World!</h1>
+        </>
+    )
+}
+
+export default App
